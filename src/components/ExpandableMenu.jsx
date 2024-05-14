81ed1a46da71da36c7784f9ebb6f3207511243dd
hkshakib <humayunkibriashakib@gmail.com>
2024-05-14 13:16:08 +0600

    feat: Added All enu Items

--- src/components/ExpandableMenu.jsx
+++ src/components/ExpandableMenu.jsx
@@ -1,27 +1,59 @@
+import { useState } from "react";
 import { MenuData } from "../data/MenuData";
+import { MdOutlineExpandMore } from "react-icons/md";
 
 const ExpandableMenu = () => {
+  const [expanded, setExpanded] = useState(null);
+
+  const toggleExpand = (index) => {
+    if (expanded === index) {
+      setExpanded(null);
+    } else {
+      setExpanded(index);
+    }
+  };
+
   return (
-    <section className="flex flex-col lg:flex-row bg-emerald-800 p-16 min-h-screen gap-4 flex-wrap">
+    <div className="m-2 space-y-2">
       {MenuData.map((category, index) => (
-        <label key={index} className="flex">
-          <input className="peer/showLabel absolute scale-0" type="checkbox" />
-          <span className="block text-center max-h-14 max-w-xs overflow-hidden rounded-lg bg-emerald-100 px-4 py-0 text-cyan-800 shadow-lg transition-all duration-300 peer-checked/showLabel:max-h-full">
-            <h3 className="flex h-14 cursor-pointer items-center font-bold text-[18px]">
-              {category.name}
-            </h3>
-            <div className="flex flex-col gap-2 mb-2 text-[16px]">
-              {category.items.map((item, itemIndex) => (
-                <div key={itemIndex} className="flex gap-4">
-                  <span>{item.name}</span>
-                  <span>{item.price}</span>
-                </div>
-              ))}
-            </div>
-          </span>
-        </label>
+        <div
+          key={index}
+          className="flex flex-col gap-2 rounded-lg bg-[#FFE7E7] p-5"
+          tabIndex={index + 1}
+        >
+          <div
+            className="flex cursor-pointer items-center justify-between"
+            onClick={() => toggleExpand(index + 1)}
+          >
+            <span className="text-[24px] lg:text-[36px] ">{category.name}</span>
+            <MdOutlineExpandMore
+              className={`h-10 w-10 transition-all duration-500 ${
+                expanded === index + 1 ? "rotate-180" : ""
+              }`}
+            />
+          </div>
+          <div
+            className={`overflow-hidden transition-all ${
+              expanded === index + 1
+                ? "visible max-h-screen opacity-100 duration-1000"
+                : "invisible max-h-0 opacity-0"
+            }`}
+          >
+            {category.items.map((item, itemIndex) => (
+              <div
+                key={itemIndex}
+                className={`flex gap-24 text-[24px] items-center ${
+                  itemIndex % 2 === 0 ? "border-b-2 border-[#944E63]" : "border-b-2 border-[#944E63]"
+                }`}
+              >
+                <span className="flex-1">{item.name}</span>
+                <span>{item.price}</span>
+              </div>
+            ))}
+          </div>
+        </div>
       ))}
-    </section>
+    </div>
   );
 };
 
